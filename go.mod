e8e82dd602e2edbb578a1cbe837e3fd2c847bb3e
Maxim Sikanov <max.4251474@yandex.ru>
2024-03-18 16:00:28 +0300

    test: add mocks

--- go.mod
+++ go.mod
@@ -3,14 +3,15 @@ module github.com/Max425/film-library.git
 go 1.21.1
 
 require (
+	github.com/go-redis/redismock/v9 v9.2.0
 	github.com/google/uuid v1.6.0
 	github.com/jmoiron/sqlx v1.3.5
-	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/mailru/easyjson v0.7.6
 	github.com/pkg/errors v0.9.1
 	github.com/redis/go-redis/v9 v9.5.1
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/http-swagger v1.3.4
 	github.com/swaggo/swag v1.8.1
 	go.uber.org/zap v1.27.0
@@ -19,6 +20,7 @@ require (
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
@@ -26,6 +28,7 @@ require (
 	github.com/go-openapi/spec v0.20.6 // indirect
 	github.com/go-openapi/swag v0.19.15 // indirect
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang/mock v1.6.0 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
@@ -33,6 +36,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/otiai10/copy v1.14.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
--- internal/http-server/handler/film_test.go
+++ internal/http-server/handler/film_test.go
@@ -1,85 +1,119 @@
-package handler
+package handler_test
 
 //
 //import (
-//	"bytes"
-//	"context"
+//	"encoding/json"
 //	"errors"
-//	"github.com/Max425/wbschool_exam_L2/tree/main/develop/dev11/internal/model/core"
-//	"github.com/Max425/wbschool_exam_L2/tree/main/develop/dev11/internal/service"
-//	"github.com/Max425/wbschool_exam_L2/tree/main/develop/dev11/internal/service/mocks"
-//	"github.com/golang/mock/gomock"
-//	"github.com/stretchr/testify/assert"
+//	"github.com/Max425/film-library.git/mocks/service"
 //	"net/http"
 //	"net/http/httptest"
 //	"testing"
 //	"time"
+//
+//	"github.com/Max425/film-library.git/internal/domain"
+//	"github.com/Max425/film-library.git/internal/http-server/handler"
+//	"github.com/Max425/film-library.git/internal/http-server/handler/dto"
+//	"github.com/golang/mock/gomock"
+//	"github.com/stretchr/testify/assert"
+//	"go.uber.org/zap"
 //)
 //
-//func TestHandler_createEvent(t *testing.T) {
-//	dt, _ := time.Parse("2006-01-02", "2024-09-09")
-//	mockEvent := core.Event{
-//		Date:   dt,
-//		Title:  "test",
-//		UserID: "1",
-//	}
-//	eventJSON := `{"date": "2024-09-09", "title": "test", "user_id":"1"}`
-//	tests := []struct {
-//		name                 string
-//		requestBody          string
-//		mockBehavior         func(r *mock_service.MockEvent)
-//		expectedStatusCode   int
-//		expectedResponseBody string
-//	}{
-//		{
-//			name:        "Event Create",
-//			requestBody: eventJSON,
-//			mockBehavior: func(r *mock_service.MockEvent) {
-//				r.EXPECT().CreateEvent(&mockEvent).Return(1, nil)
-//			},
-//			expectedStatusCode:   http.StatusOK,
-//			expectedResponseBody: `{"result":1}`,
-//		},
-//		{
-//			name:        "invalid input body",
-//			requestBody: `{"date": "2024-09-09"}`,
-//			mockBehavior: func(r *mock_service.MockEvent) {
-//			},
-//			expectedStatusCode:   http.StatusBadRequest,
-//			expectedResponseBody: `{"error":"invalid input body"}`,
-//		},
-//		{
-//			name:        "Error",
-//			requestBody: eventJSON,
-//			mockBehavior: func(r *mock_service.MockEvent) {
-//				r.EXPECT().CreateEvent(&mockEvent).Return(0, errors.New("some error"))
-//			},
-//			expectedStatusCode:   http.StatusInternalServerError,
-//			expectedResponseBody: `{"error":"some error"}`,
-//		},
-//	}
+//func TestFilmHandler_GetAllFilms(t *testing.T) {
+//	ctrl := gomock.NewController(t)
+//	defer ctrl.Finish()
 //
-//	for _, test := range tests {
-//		t.Run(test.name, func(t *testing.T) {
-//			ctrl := gomock.NewController(t)
-//			defer ctrl.Finish()
+//	mockFilmService := mock_handler.NewMockFilmService(ctrl)
+//	logger := zap.NewNop()
+//	filmHandler := handler.NewFilmHandler(logger, mockFilmService)
 //
-//			mockEventService := mock_service.NewMockEvent(ctrl)
-//			test.mockBehavior(mockEventService)
+//	t.Run("Success", func(t *testing.T) {
+//		mockFilms := []*domain.Film{
+//			domain.NewFilm(1, "Film 1", "Description 1", time.Now(), 8.5, nil),
+//			{ID: 2, Title: "Film 2", Rating: 7.9, ReleaseDate: "2024-03-19"},
+//			{ID: 3, Title: "Film 3", Rating: 6.8, ReleaseDate: "2024-03-20"},
+//		}
 //
-//			ctx := context.Background()
-//			services := &service.Service{Event: mockEventService}
-//			handler := Handler{services: services}
+//		mockFilmService.EXPECT().GetAllFilms(gomock.Any(), "", "").Return(mockFilms, nil)
 //
-//			mux := http.NewServeMux()
-//			mux.HandleFunc("/create_event", handler.createEvent)
+//		req := httptest.NewRequest("GET", "/api/films", nil)
+//		w := httptest.NewRecorder()
+//		filmHandler.GetAllFilms(w, req)
 //
-//			w := httptest.NewRecorder()
-//			req := httptest.NewRequest("POST", "/create_event", bytes.NewBufferString(test.requestBody))
-//			mux.ServeHTTP(w, req.WithContext(ctx))
+//		resp := w.Result()
+//		assert.Equal(t, http.StatusOK, resp.StatusCode)
 //
-//			assert.Equal(t, test.expectedStatusCode, w.Code)
-//			assert.Equal(t, test.expectedResponseBody, w.Body.String())
-//		})
-//	}
+//		var films []*dto.Film
+//		err := json.NewDecoder(resp.Body).Decode(&films)
+//		assert.NoError(t, err)
+//
+//		assert.Equal(t, 3, len(films))
+//		assert.Equal(t, 1, films[0].ID)
+//		assert.Equal(t, "Film 1", films[0].Title)
+//		assert.Equal(t, 8.5, films[0].Rating)
+//		assert.Equal(t, "2024-03-18", films[0].ReleaseDate)
+//	})
+//
+//	t.Run("InternalError", func(t *testing.T) {
+//		mockFilmService.EXPECT().GetAllFilms(gomock.Any(), "", "").Return(nil, errors.New("internal error"))
+//
+//		req := httptest.NewRequest("GET", "/api/films", nil)
+//		w := httptest.NewRecorder()
+//		filmHandler.GetAllFilms(w, req)
+//
+//		resp := w.Result()
+//		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+//	})
+//}
+//
+//func TestFilmHandler_GetAllFilms_WithSorting(t *testing.T) {
+//	ctrl := gomock.NewController(t)
+//	defer ctrl.Finish()
+//
+//	mockFilmService := mock_handler.NewMockFilmService(ctrl)
+//	logger := zap.NewNop()
+//	filmHandler := handler.NewFilmHandler(logger, mockFilmService)
+//
+//	t.Run("SortByTitle", func(t *testing.T) {
+//		mockFilms := []*domain.Film{
+//			{ID: 1, Title: "Film A", Rating: 8.5, ReleaseDate: "2024-03-18"},
+//			{ID: 2, Title: "Film B", Rating: 7.9, ReleaseDate: "2024-03-19"},
+//			{ID: 3, Title: "Film C", Rating: 6.8, ReleaseDate: "2024-03-20"},
+//		}
+//
+//		mockFilmService.EXPECT().GetAllFilms(gomock.Any(), "title", "asc").Return(mockFilms, nil)
+//
+//		req := httptest.NewRequest("GET", "/api/films?sort_by=title&order=asc", nil)
+//		w := httptest.NewRecorder()
+//		filmHandler.GetAllFilms(w, req)
+//
+//		resp := w.Result()
+//		assert.Equal(t, http.StatusOK, resp.StatusCode)
+//
+//		var films []*dto.Film
+//		err := json.NewDecoder(resp.Body).Decode(&films)
+//		assert.NoError(t, err)
+//
+//		assert.Equal(t, 3, len(films))
+//		assert.Equal(t, "Film A", films[0].Title)
+//		assert.Equal(t, "Film B", films[1].Title)
+//		assert.Equal(t, "Film C", films[2].Title)
+//	})
+//
+//	t.Run("InvalidSortOrder", func(t *testing.T) {
+//		req := httptest.NewRequest("GET", "/api/films?sort_by=title&order=invalid", nil)
+//		w := httptest.NewRecorder()
+//		filmHandler.GetAllFilms(w, req)
+//
+//		resp := w.Result()
+//		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+//	})
+//
+//	t.Run("InvalidSortByField", func(t *testing.T) {
+//		req := httptest.NewRequest("GET", "/api/films?sort_by=invalid&order=asc", nil)
+//		w := httptest.NewRecorder()
+//		filmHandler.GetAllFilms(w, req)
+//
+//		resp := w.Result()
+//		assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+//	})
 //}
